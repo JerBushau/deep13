1385e47f6dd5b1eafe7f304ea8b40aa96dc67d64
JerBushau <21266962+JerBushau@users.noreply.github.com>
2022-05-19 23:45:11 -0400

    Set paths

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build'
+    }),
     prerender: {
 			default: true
 		}
